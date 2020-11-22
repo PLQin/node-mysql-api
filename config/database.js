f4ce07452d3f1126a01a00cb33f5e5dbeb330e93
Raymond <773561801@qq.com>
2020-11-22 23:15:28 +0800

    Update 2020年11月22日 23:15:17Update 2020年11月22日 23:15:17

--- config/database.js
+++ config/database.js
@@ -5,6 +5,10 @@ const config = require('./index.js');
 // 创建一个连接池并直接使用它
 // 解决了 express mysql Error: Cannot enqueue Query after fatal error. 的问题
 // 见： https://github.com/mysqljs/mysql#pooling-connections
+// 其他参考资料： 
+//    - [Node.js与数据库](https://itbilu.com/nodejs/npm/NyPG8LhlW.html#pool)
+//    - [Node.js与连接池](https://blog.csdn.net/wb_001/article/details/79000522)
+//    - [服务器关闭连接](https://stackoverflow.com/questions/13018227/reproduce-mysql-error-the-server-closed-the-connection-node-js)
 let pool = mysql.createPool(Object.assign(config, {
 
   // 启用后，您可以通过用分号 multipleStatements 分隔每个语句来对多个语句执行查询。结果将是每个语句的数组。
@@ -27,6 +31,9 @@ let connection = (res, sql, params) => {
         // reject(err)
         res.json({ code: 500, message: err.code });
         connection.release()
+
+        console.error(`CONNECTION ERROR : ----------------------------`);
+        console.error(err);
         return
       }
 
@@ -35,6 +42,9 @@ let connection = (res, sql, params) => {
           // reject(err)
           res.json({ code: 500, message: err.code });
           connection.release()
+
+          console.error(`SQL ERROR : ----------------------------`);
+          console.error(err);
           return
         }
 
